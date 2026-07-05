b776925fb9e4d74fe5a8d0a0f3eb8e0527525e5b
devanshu <devanshukothari26@gmail.com>
2026-07-05 13:16:53 +0530

    Security: Remove cookie maxAge to enable auto-logout on browser close

--- gateway/server.js
+++ gateway/server.js
@@ -34,8 +34,7 @@ app.use(session({
     cookie: {
         secure: true,
         sameSite: 'none',
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000
+        httpOnly: true
     }
 }));
 
